c4bb043453e5701333fe4bd30be9f077d27ec3f3
Sheldon Phillips <sheldon.phillips1991@gmail.com>
2021-01-16 15:20:16 -0500

    Added gitignore to bashscript

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 rm -fv ~/.bashrc
 rm -fv ~/.gitconfig
+rm -fv ~/.gitignore
 rm -fv ~/.inputrc
 rm -fv ~/.profile
 rm -fv ~/.tmux.conf
@@ -9,6 +10,7 @@ rm -fv ~/.vimrc
 
 ln -vs ~/etc/bashrc ~/.bashrc
 ln -vs ~/etc/gitconfig ~/.gitconfig
+ln -vs ~/etc/gitignore ~/.gitignore
 ln -vs ~/etc/inputrc ~/.inputrc
 ln -vs ~/etc/profile ~/.profile
 ln -vs ~/etc/tmux.conf ~/.tmux.conf
