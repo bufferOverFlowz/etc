40dc8007b7a03f80c87735af2bd115b23ce91e9c
Sheldon Phillips <sheldon.phillips1991@gmail.com>
2021-01-16 15:09:32 -0500

    Added newline to setup.bat to account for new gitiqnore

--- setup.bat
+++ setup.bat
@@ -2,8 +2,10 @@
 
 del /f /q C:\Users\%USERNAME%\.gitconfig
 del /f /q C:\Users\%USERNAME%\_vimrc
+del /f /q C:\Users\%USERNAME%\.gitignore
 
 copy C:\Users\%USERNAME%\etc\gitconfig C:\Users\%USERNAME%\.gitconfig
 copy C:\Users\%USERNAME%\etc\vimrc C:\Users\%USERNAME%\_vimrc
+copy C:\Users\%USERNAME%\etc\gitignore C:\Users\%USERNAME%\.gitignore
 echo Copy Success!
 pause
