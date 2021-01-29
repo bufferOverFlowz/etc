a83ae834acf25c9a002244ec0a3408f95b825e95
Sheldon Phillips <sheldon.phillips1991@gmail.com>
2021-01-29 16:31:54 -0500

    Removed _vimrc. Just pull from same file use different copy outputs

--- setup.bat
+++ setup.bat
@@ -13,5 +13,13 @@ xcopy C:\Users\%USERNAME%\etc\vim\templates C:\Users\%USERNAME%\.vim\templates
 copy C:\Users\%USERNAME%\etc\gitconfig C:\Users\%USERNAME%\.gitconfig
 copy C:\Users\%USERNAME%\etc\vimrc C:\Users\%USERNAME%\_vimrc
 copy C:\Users\%USERNAME%\etc\gitignore C:\Users\%USERNAME%\.gitignore
+
 echo Copy Success!
+echo Checking for vimfiles.
+
+if not exist C:\Users\%USERNAME%\.vim mkdir C:\Users\%USERNAME%\.vim
+
+if exist C:\Users\%USERNAME%\vimfiles move /y C:\Users\%USERNAME%\.vim C:\Users\%USERNAME%\vimfiles\* 
+
+echo Script Complete. Exiting.
 pause
